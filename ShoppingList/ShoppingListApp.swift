f3e500c58255f642bbdd8412f2b9a9b5da5322a4
Brian Echeozo <bibsterfal@gmai.com>
2025-04-18 13:14:33 -0600

    Initial Commit

---
+++ ShoppingList/ShoppingListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShoppingListApp.swift
+//  ShoppingList
+//
+//  Created by Brian Echeozo on 2025-04-18.
+//
+
+import SwiftUI
+
+@main
+struct ShoppingListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
